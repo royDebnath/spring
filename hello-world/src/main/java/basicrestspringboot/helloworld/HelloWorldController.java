88f65746e088f84b6429e9cfe6d4b69b2fb49c7b
Debnath Roy <xdebroy@gmail.com>
2020-12-16 12:56:11 +0530

    Added basic spring boot hello-world

---
+++ hello-world/src/main/java/basicrestspringboot/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package basicrestspringboot.helloworld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String retrieveAllUsers() {
+        return "Hello World!";
+    }
+}
